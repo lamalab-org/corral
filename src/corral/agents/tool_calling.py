806b284bf1408b7541a68d6d1e01dfd2b682102d
n0w0f <pvt.nawaf@gmail.com>
2025-07-04 15:39:39 +0200

    fix: revert to task prompt

--- src/corral/agents/tool_calling.py
+++ src/corral/agents/tool_calling.py
@@ -122,7 +122,7 @@ class ToolCallingAgent:
             interface.get_available_tools_for_task(task_id)
         )
         if task_prompt is None:
-            task_guide = interface.get_task_guide(task_id)
+            task_guide = interface.get_task_prompt(task_id)
         else:
             task_guide = task_prompt
 
