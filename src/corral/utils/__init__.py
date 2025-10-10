bba64160c744ccc0c0c96219dedc1d1b60038343
MrtinoRG <martinriosgarcia@gmail.com>
2025-10-10 19:30:36 +0200

    feat: add terminal tools, context7 tool and Docker container info

--- src/corral/utils/__init__.py
+++ src/corral/utils/__init__.py
@@ -1,11 +1,23 @@
-from . import code_tools, io_tools, modal, rag, search_tools, task_group, tool_helpers
+from . import (
+    code_tools,
+    context7_tools,
+    io_tools,
+    modal,
+    rag,
+    task_group,
+    terminal_tools,
+    tool_helpers,
+    web_search_tools,
+)
 
 __all__ = [
     "code_tools",
+    "context7_tools",
     "io_tools",
     "modal",
     "rag",
-    "search_tools",
     "task_group",
+    "terminal_tools",
     "tool_helpers",
+    "web_search_tools",
 ]
