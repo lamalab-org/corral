a4cc00dba332ded84b38fb9d0626aebfd0a58dc7
imandal98 <indrajeetmandal.aaa@gmail.com>
2025-11-15 18:08:40 +0530

    feat: add runs of task2 claude react

--- reports/afm/run_ReActAgent_gpt_4o.py
+++ reports/afm/run_ReActAgent_gpt_4o.py
@@ -56,7 +56,7 @@ if __name__ == "__main__":
         logger.info(f"Running benchmark with verbosity: {verbose}")
         try:
             model = "gpt-4o-2024-08-06"
-            run_name = f"gpt-4o-React-task_4-{verbose}_verbosity"
+            run_name = f"gpt-4o-React-task_2-{verbose}_verbosity"
             run_benchmark(model=model, run_name=run_name, verbose=verbose, temperature=0)
 
         except Exception as e:
